ae66f87283bfa17bbb92330734bffacbd2b6af88
Abdallah4021 <42907516+Abdallah4021@users.noreply.github.com>
2020-03-02 16:15:19 +0200

    Initial Commit

---
+++ CocoaPodsPrintDemo/CocoaPodsPrintDemo.h
@@ -0,0 +1,19 @@
+//
+//  CocoaPodsPrintDemo.h
+//  CocoaPodsPrintDemo
+//
+//  Created by Abdallah on 3/2/20.
+//  Copyright © 2020 Abdallah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaPodsPrintDemo.
+FOUNDATION_EXPORT double CocoaPodsPrintDemoVersionNumber;
+
+//! Project version string for CocoaPodsPrintDemo.
+FOUNDATION_EXPORT const unsigned char CocoaPodsPrintDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPodsPrintDemo/PublicHeader.h>
+
+
